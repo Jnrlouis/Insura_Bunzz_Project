6ac17260b2f9fadb53d9c97f21cfabc86e40e4f2
JnrLouis <chibuezelouis@gmail.com>
2023-02-25 00:27:39 +0100

    fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/InsuraApp" element={<InsuraApp />} />
+        <Route path="insuraApp" element={<InsuraApp />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
